dd241fd6d9bfc491bd812dd2ebe30ad8af5302c2
Dylan Mazur Zgainer <136861567+DylanMZ05@users.noreply.github.com>
2024-02-17 21:00:11 -0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,10 @@
-var check = document.querySelector (".check");
-check.addEventListener('click',idioma);
-
-function idioma(){
-    let id=check.checked;
-    if (id==true) {
-        location.href="/en/index.html";
-    }
-    else {
-        location.href = "../index.html";
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var checkbox = document.querySelector('.check');
+    var link = document.querySelector('.link-style');
+    
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // Evitar la acción predeterminada del enlace
+        checkbox.checked = !checkbox.checked; // Alternar el estado del checkbox
+        // Aquí puedes agregar más acciones si lo deseas, como cambiar estilos o redirigir
+    });
+});
